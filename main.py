db6d0f78f101fbc55d5309c1609959fdf4ad3fa1
Kacper Jabłoński <kacjab171@gmail.com>
2022-06-16 10:33:41 +0200

    SSIM and PSNR comparison working fine

---
+++ main.py
@@ -0,0 +1,39 @@
+from math import log10, sqrt
+import cv2
+from skimage.metrics import structural_similarity
+import imutils
+import numpy as np
+
+
+def PSNR(original, compressed):
+    mse = np.mean((original - compressed) ** 2)
+    if (mse == 0):  # MSE is zero means no noise is present in the signal .
+        # Therefore PSNR have no importance.
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * log10(max_pixel / sqrt(mse))
+    return psnr
+
+def SSIM(original, compressed):
+    grayA = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    grayB = cv2.cvtColor(compressed, cv2.COLOR_BGR2GRAY)
+
+    (ssim, diff) = structural_similarity(grayA, grayB, full=True)
+    diff = (diff * 255).astype("uint8")
+
+    # print("SSIM: {}".format(ssim))
+    return ssim
+
+
+def main():
+    original = cv2.imread("original.jpg")
+    compressed = cv2.imread("compressed.png", 1)
+
+    psnr = PSNR(original, compressed)
+    ssim = SSIM(original, compressed)
+
+    print(f"PSNR value is {psnr} dB and SSIM: {format(ssim)}")
+
+
+if __name__ == "__main__":
+    main()
