7a24036cbc3b298f4def4e671d207343888640a3
dtymek <dtymek@onet.pl>
2022-09-20 18:40:47 +0200

    commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import math
 from math import log10, sqrt
+import os
 import cv2
 from skimage.metrics import structural_similarity
 import imutils
 import numpy as np
 
-
-def PSNR(original, compressed):
+def PSNR(begin, sr):
+    original = cv2.imread(begin)
+    compressed = cv2.imread(sr)
     mse = np.mean((original - compressed) ** 2)
     if (mse == 0):  # MSE is zero means no noise is present in the signal .
         # Therefore PSNR have no importance.
@@ -15,7 +18,11 @@ def PSNR(original, compressed):
     return psnr
 
 
-def SSIM(original, compressed):
+def SSIM(begin, sr):
+    original = cv2.imread(begin)
+    compressed = cv2.imread(sr)
+    #print(begin)
+    #print(sr)
     grayA = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
     grayB = cv2.cvtColor(compressed, cv2.COLOR_BGR2GRAY)
 
@@ -25,18 +32,66 @@ def SSIM(original, compressed):
     # print("SSIM: {}".format(ssim))
     return ssim
 
+def psnr_for_files(original_dir, completed_dir):
+    original_files = os.listdir(original_dir)
+    completed_files = os.listdir(completed_dir)
+    psnrs = []
+    for file in original_files:
+        name = os.path.splitext(file)[0]
+        psnr = PSNR(original_dir + "/" + file, completed_dir + "/" + name + '.png')
+        psnrs.append(psnr)
+
+    return psnrs
+
+
+def ssim_for_files(original_dir, completed_dir):
+    original_files = os.listdir(original_dir)
+    completed_files = os.listdir(completed_dir)
+    ssims = []
+    for file in original_files:
+        name = os.path.splitext(file)[0]
+        ssim = SSIM(original_dir + "/" + file, completed_dir + "/" + name + '.png')
+        ssims.append(ssim)
+
+    return ssims
+
+def variance(data, ddof=0):
+    n = len(data)
+    mean = sum(data) / n
+    return sum((x - mean) ** 2 for x in data) / (n - ddof)
+
+
+def stdev(data):
+    var = variance(data)
+    std_dev = math.sqrt(var)
+    return std_dev
+
 
 def main():
-    # foler = open("original")  # image0000.jpeg, image0001.png
-    # folderze2 = open("constructed/")  # image0000.png, image0001.png
-    original = cv2.imread("original.jpg")
-    compressed = cv2.imread("compressed.png", 1)
+    psnr = psnr_for_files("IMAGES/ORIG", "IMAGES/SR")
+    ssim = ssim_for_files("IMAGES/ORIG", "IMAGES/SR")
 
-    psnr = PSNR(original, compressed)
-    ssim = SSIM(original, compressed)
+    print(psnr)
+    print(ssim)
 
-    print(f"PSNR value is {psnr} dB and SSIM: {format(ssim)}")
+    print("psnr")
+    print(variance(psnr))
+    print(stdev(psnr))
+    print(max(psnr))
+    print(min(psnr))
+    print(sum(psnr)/len(psnr))
+
+
+    print("ssim")
+    print(variance(ssim))
+    print(stdev(ssim))
+    print(max(ssim))
+    print(min(ssim))
+    print(sum(ssim)/len(ssim))
+
+
 
 
 if __name__ == "__main__":
     main()
+
